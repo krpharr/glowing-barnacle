5bb67374a7467fc978b6512e371a082f0e06b05e
rpharr <rpharr@richmond.edu>
2020-03-23 19:29:34 -0400

    added basic routing and bootstrap

---
+++ src/pages/Directory.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+class Directory extends React.Component {
+    state = {
+
+    };
+
+    render(){
+        return (
+            <div>
+                <h1>Directory</h1>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+                    Similique at esse voluptates architecto, consequatur sed labore, 
+                    dolorum praesentium ipsum ut ipsam consequuntur quo eius eveniet autem 
+                    doloribus quis veritatis voluptas!
+                </p>
+            </div>
+        );
+    }
+}
+
+export default Directory;
