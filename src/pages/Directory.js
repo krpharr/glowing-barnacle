3b72afa0071218d7655ff8e33c031d4635dbe241
rpharr <rpharr@richmond.edu>
2020-03-24 15:56:05 -0400

    added sort and filter functions and added departments array to state

--- src/pages/Directory.js
+++ src/pages/Directory.js
@@ -1,37 +1,57 @@
 import React from "react";
 import data from "../data/data";
 
-console.log(data);
-
 class Directory extends React.Component {
     state = {
-        employees: data
+        employees: data,
+        departments: []
     };
 
     componentDidMount() {
+        const departments = [...new Set(this.state.employees.map(emp => emp.department))];
+        this.setState({departments: departments});
         
     }
 
+    sort = (column) => {
+        let sorted = this.state.employees.slice();
+        sorted.sort((a,b)=>{
+            if(a[column] > b[column])return 1;
+            if(a[column] < b[column])return -1;
+            return 0;
+        });
+        return sorted;
+    };
+
+    filter = (department) => {
+        let filtered = this.state.employees.map(emp => {
+            return emp.department === department;
+        });
+        return filtered;
+    };
+
     mapEmployees = () =>  {
-        let empTable = this.state.employees.map(emp => {
+        console.log(this.state.departments);
+        let empListItems = this.sort("department").map(emp => {
             return (
-                <div>
+                <li key={emp.id}> 
                     <img alt={emp.email} src={emp.avatar}></img>
                     <h2>{emp.firstName} {emp.lastName}</h2>
                     <h4>{emp.department} | {emp.title}</h4>
                     <span>{emp.email} {emp.phone}</span>
-                </div>
+                </li>
             );
         });
-        return empTable;
+        return empListItems;
     };
     
-
     render(){
         return (
             <div>
                 <h1>Directory</h1>
-                {this.mapEmployees()}
+                <ul>
+                    {this.mapEmployees()}
+                </ul>
             </div>
         );
     }
